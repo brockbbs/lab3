19d8bf9bd714d619b9626f218a0782fcac3dc7f8
brockbbs <plexyt810@gmail.com>
2026-09-22 14:30:53 +0400

    решение task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,8 @@
-
+from math import *
+def y(x):
+    numerator = math.exp(-2 * x) * math.pi * (x ** 2 + math.sqrt(x + 5))
+    inner = abs(math.exp(x) - 2 * math.log(x))
+    denominator = math.sqrt(abs(2 * x - math.sqrt(inner)))
+    return numerator / denominator
+x = 0.5
+print(f"y({x}) = {y(x):.5f}")
